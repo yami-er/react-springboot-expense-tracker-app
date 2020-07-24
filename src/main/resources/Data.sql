9a334ebeadf6f084977d3abf88fb363229d710de
yami-er <queenyani@gmail.com>
2020-07-24 09:04:02 -0400

    commit after modification in sql table

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -8,6 +8,6 @@ insert into category values (2,'Auto Loan')
 insert into category  values (3,'Travel')
 
 
-insert into expense values (100,'New York Business Trip','2019-06-16T17:00:00.000Z','New York',1,1)
-insert into expense values (101,'Ford Mustang Payment','2019-06-15T15:00:00.000Z','Los Angeles',2,2)
-insert into expense values(102,'Grand Canyon Trip With Family','2019-06-15T15:00:00.000Z','Arizona',3,1)
+insert into expense values (100,'Ethiopia travel','2019-06-16T17:00:00.000Z','Ethiopia',1,1)
+insert into expense values (101,'Car rental','2019-06-15T15:00:00.000Z','Ethiopia',2,2)
+insert into expense values(102,'Trip to DC','2019-06-15T15:00:00.000Z','DC',3,1)
