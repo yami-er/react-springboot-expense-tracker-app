b0220fab2456679231bf27488eea57ea66042a0e
yami-er <queenyani@gmail.com>
2020-07-24 00:57:14 -0400

    Updates

---
+++ precache-manifest.8cbe791d513716c797e972d776a2fd1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8cd6ecf9fbdae9a4017c9a7bc7eb07",
+    "url": "/react-springboot-expense-tracker-app/index.html"
+  },
+  {
+    "revision": "8ee2854c3da151a2de42",
+    "url": "/react-springboot-expense-tracker-app/static/css/2.fea45d51.chunk.css"
+  },
+  {
+    "revision": "bdf4113ac90efa96c383",
+    "url": "/react-springboot-expense-tracker-app/static/css/main.5d79c63e.chunk.css"
+  },
+  {
+    "revision": "8ee2854c3da151a2de42",
+    "url": "/react-springboot-expense-tracker-app/static/js/2.d0dba39a.chunk.js"
+  },
+  {
+    "revision": "ce8ac3c5d9da37689de19b45a8b43043",
+    "url": "/react-springboot-expense-tracker-app/static/js/2.d0dba39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf4113ac90efa96c383",
+    "url": "/react-springboot-expense-tracker-app/static/js/main.93091f00.chunk.js"
+  },
+  {
+    "revision": "0fa618e359dffd784f93",
+    "url": "/react-springboot-expense-tracker-app/static/js/runtime-main.ee94c789.js"
+  }
+]);
